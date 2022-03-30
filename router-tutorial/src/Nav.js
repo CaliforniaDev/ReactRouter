ee9b28d228c62e3775635238b21270827909dd8a
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-03-29 23:10:40 -0700

    Add and export nav

---
+++ router-tutorial/src/Nav.js
@@ -0,0 +1,14 @@
+const Nav = () => {
+  return (
+    <nav>
+      <h3>Logo</h3>
+      <ul>
+        <li>App</li>
+        <li>Profile</li>
+        <li>Friends</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
